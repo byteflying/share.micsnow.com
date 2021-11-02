10870e44edc016860b7e8665c9eb3c29de52aa0a
microsnow <43006026+byteflying@users.noreply.github.com>
2021-11-02 09:39:10 +0800

    Add files via upload

---
+++ js/site.js
@@ -0,0 +1,53 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+//去除字符串头尾空格或指定字符  
+String.prototype.trim = function (c) {
+    if (c == null || c == "") {
+        var str = this.replace(/^s*/, '');
+        var rg = /s/;
+        var i = str.length;
+        while (rg.test(str.charAt(--i)));
+        return str.slice(0, i + 1);
+    }
+    else {
+        var rg = new RegExp("^" + c + "*");
+        var str = this.replace(rg, '');
+        rg = new RegExp(c);
+        var i = str.length;
+        while (rg.test(str.charAt(--i)));
+        return str.slice(0, i + 1);
+    }
+}
+//去除字符串头部空格或指定字符  
+String.prototype.trimStart = function (c) {
+    if (c == null || c == "") {
+        var str = this.replace(/^s*/, '');
+        return str;
+    }
+    else {
+        var rg = new RegExp("^" + c + "*");
+        var str = this.replace(rg, '');
+        return str;
+    }
+}
+
+//去除字符串尾部空格或指定字符  
+String.prototype.trimEnd = function (c) {
+    if (c == null || c == "") {
+        var str = this;
+        var rg = /s/;
+        var i = str.length;
+        while (rg.test(str.charAt(--i)));
+        return str.slice(0, i + 1);
+    }
+    else {
+        var str = this;
+        var rg = new RegExp(c);
+        var i = str.length;
+        while (rg.test(str.charAt(--i)));
+        return str.slice(0, i + 1);
+    }
+}
